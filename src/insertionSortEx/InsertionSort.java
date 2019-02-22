a0322351085c1308bc287f8102581343fbb3bd2c
dydwlsdlz <dydwlsdlz0527@gmail.com>
2019-02-22 16:53:54 +0900

    삽입 정렬

--- src/insertionSortEx/InsertionSort.java
+++ src/insertionSortEx/InsertionSort.java
@@ -5,7 +5,7 @@ public class InsertionSort {
 	private static void insertionsort(int[] arr, int n) {
 		int key;
 		//0 번째 인덱스는 이미 정렬된 상태로 보고, 1 번째 인덱스부터 시작한다.
-		for(int i=1;i<n-1;i++) {
+		for(int i=1;i<n;i++) {
 			//삽입될 숫자인 i번째 인덱스를 key변수에 대입한다.
 			key = arr[i];
 			int j=i-1;
